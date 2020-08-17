f6ead6bfb3d5b2ebaea521df6cb0f37cde148733
Fizx <kazanfizuku@gmail.com>
2020-08-17 11:50:43 +0800

    Update role.js

--- commands/tools/role.js
+++ commands/tools/role.js
@@ -1,30 +1,22 @@
 const Discord = require('discord.js')
-const bot = new Discord.Client({disableEveryone: true});
-const db = require("quick.db"); 
 
 module.exports = {
-    name: 'role',
-    description: "Repeats what the user said.",
-    run: async (bot, message, args) => {
-      
-      let channel;
-  
-        let channels = await db.fetch(`channel_${message.guild.id}`)
+  name: 'role',
+  description: "Repeats what the user said.",
+  run: async (bot, message, args) => {
+     const db = bot.db
+      let channel = await db.fetch(`channel_${message.guild.id}`)
     
-        if(channels == null){
-           channel = message.channel.name;
-        } else {
-           channel = channels;
-        }
+      if(channel == null) channel = message.channel.name;
       
-        if(!message.member.hasPermission('MANAGE_ROLES')) return message.reply("you don't have **Manage Roles** permissions to do this command!")
-        if(!message.guild.me.hasPermission("MANAGE_ROLES")) return message.reply(`I do not have the **Manage Roles** permission to use this command!`)
-   var member = message.mentions.members.first();
-       if(!member) return message.reply("who should I give the role to?");
-   var roler = args.slice(2).join(" ")
-   var role = message.guild.roles.find(role => role.name === `${roler}`);
+      if(!message.member.hasPermission('MANAGE_ROLES')) return message.reply("you don't have **Manage Roles** permissions to do this command!")
+      if(!message.guild.me.hasPermission("MANAGE_ROLES")) return message.reply(`I do not have the **Manage Roles** permission to use this command!`)
+      var member = message.mentions.members.first();
+      if(!member) return message.reply("who should I give the role to?");
+      var roler = args.slice(2).join(" ")
+      var role = message.guild.roles.find(role => role.name === `${roler}`);
    
-   if(!role) return message.reply("I cannot find that role!")
+      if(!role) return message.reply("I cannot find that role!")
               
        if(args[0] === "remove"){
        member.removeRole(role).catch(console.error)
